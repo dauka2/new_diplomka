b62c1ef3a6707c6883168d0d9f5196b01cf498af
must_dilnaz <must.dilnaz@gmail.com>
2024-06-10 15:28:21 +0500

    jsons

--- src/Components/Teacher/TeacherLogin.jsx
+++ src/Components/Teacher/TeacherLogin.jsx
@@ -1,105 +1,98 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import firebase from '../firebase'
-import Swal from 'sweetalert2'
-
-const baseUrl='https://minipro.pythonanywhere.com/api'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import firebase from '../firebase';
+import Swal from 'sweetalert2';
+import teachers from '../../data/teachers.json'; // Путь к вашему JSON-файлу
 
 const TeacherLogin = () => {
+  const teacherLoginStatus = localStorage.getItem('teacherLoginStatus');
+  const [teacherLoginData, setTeacherLoginData] = useState({
+    email: '',
+    password: ''
+  });
+  const [errorMsg, setErrorMsg] = useState('');
 
-    const teacherLoginStatus=localStorage.getItem('teacherLoginStatus');
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.title = 'LMS | Teacher Login';
+  }, []);
 
-      const [teacherLoginData,setTeacherLoginData]=useState({
-        email:'',
-        password:''
-      });
+  const handleChange = (event) => {
+    setTeacherLoginData({
+      ...teacherLoginData,
+      [event.target.name]: event.target.value
+    });
+  };
 
-      useEffect(() => {
-        window.scrollTo(0, 0)
-      }, [])
-      
-      const [errorMsg, setErrorMsg]=useState('')
+  const submitForm = () => {
+    const { email, password } = teacherLoginData;
 
-      const handleChange=(event)=>{
-        setTeacherLoginData({
-            ...teacherLoginData,
-            [event.target.name]:event.target.value
-        });
+    const teacher = teachers.find((teacher) => teacher.email === email && teacher.password === password);
+
+    if (teacher) {
+      localStorage.setItem('teacherLoginStatus', true);
+      localStorage.setItem('teacherId', teacher.id);
+      window.location.href = '/teacher-dashboard';
+    } else {
+      setErrorMsg('Неправильный email или пароль');
     }
+  };
 
-    const submitForm=()=>{
-        const teacherFormData=new FormData;
-        teacherFormData.append('email',teacherLoginData.email)
-        teacherFormData.append('password',teacherLoginData.password)
-        try{
-            axios.post(baseUrl+'/teacher-login',teacherFormData)
-            .then((res)=>{
-                if(res.data.bool==true){
-                    localStorage.setItem('teacherLoginStatus',true);
-                    localStorage.setItem('teacherId',res.data.teacher_id);
-                    window.location.href='/teacher-dashboard';
-                }else{
-                  if(res.status==200 || res.status==201){
-                    Swal.fire({
-                        title:'Please Enter all details correctly!',
-                        icon:'error',
-                        toast:true,
-                        timer:2000,
-                        position:'top',
-                        timerProgressBar: true,
-                        showConfirmButton: false
-                    });
-                }
-                }
-            })
-        }catch(error){
-            console.log(error)
-        }
-    }
-
-    if(teacherLoginStatus=='true'){
-        window.location.href='/teacher-dashboard';
-    }
-
-    useEffect(()=>{
-        document.title='LMS | Teacher Login'
-      })
-
+  if (teacherLoginStatus === 'true') {
+    window.location.href = '/teacher-dashboard';
+  }
 
   return (
     <>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card border-0 shadow rounded-3 my-5">
-          <div class="card-body p-4 p-sm-5">
-          <div class="text-center wow fadeInUp">
-          <h5 class="card-title text-center mb-3 fw-light fs-5 text-dark">ЛОГИН УЧИТЕЛЯ</h5>
-          </div>            
-            {errorMsg && <p className='text-danger'>{errorMsg}</p>}
-              <div class="form-floating mb-3">
-                <input type="email" value={teacherLoginData.email} onChange={handleChange} name='email' class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                <label for="floatingInput">E-mail</label>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card border-0 shadow rounded-3 my-5">
+              <div className="card-body p-4 p-sm-5">
+                <div className="text-center wow fadeInUp">
+                  <h5 className="card-title text-center mb-3 fw-light fs-5 text-dark">ЛОГИН УЧИТЕЛЯ</h5>
+                </div>
+                {errorMsg && <p className="text-danger">{errorMsg}</p>}
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    value={teacherLoginData.email}
+                    onChange={handleChange}
+                    name="email"
+                    className="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
+                  <label htmlFor="floatingInput">E-mail</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input
+                    value={teacherLoginData.password}
+                    name="password"
+                    type="password"
+                    onChange={handleChange}
+                    className="form-control"
+                    id="floatingPassword"
+                    placeholder="Password"
+                  />
+                  <label htmlFor="floatingPassword">Пароль</label>
+                </div>
+                <div className="d-grid my-4">
+                  <button onClick={submitForm} className="btn btn-primary rounded-pill btn-login text-uppercase fw-bold" type="submit">
+                    Авторизоваться
+                  </button>
+                  <hr className="" />
+                  <Link to="/teacher-register" className="btn btn-telecom rounded-pill btn-login text-uppercase fw-bold">
+                    Зарегистрироваться
+                  </Link>
+                </div>
               </div>
-              <div class="form-floating mb-3">
-                <input value={teacherLoginData.password} name='password' type="password" onChange={handleChange} class="form-control" id="floatingPassword" placeholder="Password"/>
-                <label for="floatingPassword">Пароль</label>
-              </div>
-              <div class="d-grid my-4">
-                <button onClick={submitForm} class="btn btn-primary rounded-pill btn-login text-uppercase fw-bold" type="submit" >Авторизоваться</button>
-                <hr className=''/>
-                <Link to='/teacher-register' type="submit"  class="btn btn-telecom rounded-pill btn-login text-uppercase fw-bold " >Зарегистрироваться</Link>
-              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default TeacherLogin
+export default TeacherLogin;
