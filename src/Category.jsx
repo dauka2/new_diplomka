0b104536d3a0030659e3b30ff7079113fe073577
daulet <daulet.marat.2018@mail.ru>
2024-06-12 11:22:38 +0500

    изменил чуть чуть уже хз что

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,44 +1,40 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react'
-import { useState } from 'react'
-import Swal from 'sweetalert2'
-import axios from 'axios'
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import categoriesData from '../src/data/categories.json';  // Подключаем локальный JSON файл
 
-const baseUrl='https://minipro.pythonanywhere.com/api'
+const baseUrl = 'https://minipro.pythonanywhere.com/api';
 
 const Category = () => {
-  useEffect(()=>{
-    document.title='LMS | Our Categories'
-  })
+  useEffect(() => {
+    document.title = 'LMS | Our Categories';
+  }, []);
 
-  const [categoryData, setCategoryData]=useState([]);
-
-  useEffect(()=>{
-    try{
-        axios.get(baseUrl+'/category/')
-        .then((res)=>{
-            setCategoryData(res.data)
-        });
-    }catch(error){
-        console.log(error);
-    }
-  },[]);
+  const [categoryData, setCategoryData] = useState([]);
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    try {
+      // Используем данные из локального JSON файла
+      setCategoryData(categoriesData);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className='container mt-4'>
-      <div class=" mt-3 text-center wow fadeInUp">
+      <div class="mt-3 text-center wow fadeInUp">
         <h6 class="section-title bg-white text-center text-primary px-3">Категории</h6>
         <h1 class="mb-5">Все категории</h1>
       </div>    
       <div className='row mb-4'>
-      {categoryData && categoryData.map((row,index) =>
-      <div className='col-md-3 mb-3'>
+      {categoryData && categoryData.map((row, index) =>
+      <div className='col-md-3 mb-3' key={index}>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title"><Link to={`/course/${row.id}/${row.title}`}>{row.title}  ({row.total_courses})</Link></h5>
@@ -49,7 +45,7 @@ const Category = () => {
       )}
     </div>
   </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
